4bfd91334586d2d094b108712c1a306726e06321
varanasikalyan <varanasikalyan@icloud.com>
2019-02-24 19:51:23 +0530

    remove scss
    remove scss

--- src/components/question/show/QuestionList.js
+++ src/components/question/show/QuestionList.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import QuestionCard from './QuestionCard';
 
 class QuestionList extends Component {
   render() {
     return (
       <div className="question-list section">
-        <QuestionCard />
-        <QuestionCard />
-        <QuestionCard />
       </div>
     )
   }
