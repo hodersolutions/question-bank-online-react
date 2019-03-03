e14697751aeb7a08657959c987122517c7eaab60
varanasikalyan <varanasikalyan@icloud.com>
2019-03-03 18:01:57 +0530

    404
    404

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import SignIn from "../auth/SignIn";
 import SignUp from "../auth/SignUp";
 import Dashboard from "../dashboard/Dashboard";
@@ -8,21 +8,23 @@ import CreateQuestion from "../question/create/CreateQuestion";
 import ForgotPassword from "../auth/ForgotPassword";
 import CreateModule from '../module/create/CreateModule';
 import Authenticate from '../auth/JWTAuthentication';
+import NotFound from '../common/NotFound'
 
 const AuthenticatedCreateModule = Authenticate(CreateModule);
 const AuthenticatedShowQuestion = Authenticate(ShowQuestion);
 const AuthenticatedCreateQuestion = Authenticate(CreateQuestion);
 
 const Routes = () => (
-	<div>	
+	<Switch>	
     	<Route exact path='/' component={ Dashboard } />
-		<Route path='/signin' component={ SignIn } />
-    	<Route path='/signup' component={ SignUp } />
-		<Route path='/module' component={ AuthenticatedCreateModule } />
-      	<Route path='/question' component={ AuthenticatedCreateQuestion } />
+		<Route exact path='/signin' component={ SignIn } />
+    	<Route exact path='/signup' component={ SignUp } />
+		<Route exact path='/module' component={ AuthenticatedCreateModule } />
+      	<Route exact path='/question' component={ AuthenticatedCreateQuestion } />
 		<Route exact path='/question/show' component={ AuthenticatedShowQuestion } />		
-		<Route path='/forgot/password' component={ ForgotPassword } />
-	</div>
+		<Route exact path='/forgot/password' component={ ForgotPassword } />
+		<Route path='*' component={ NotFound } />
+	</Switch>
 );
 
 export default Routes;
